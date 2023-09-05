aec5421037ae3fcde9722e23279d3c4d04e64aa3
azelenin057 <yazel057@gmail.com>
2023-09-05 20:24:57 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const form = document.querySelector(".login-form");
 
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+  const userData = { email: email.value, password: password.value };
+
+  console.log(userData);
+  event.currentTarget.reset();
+};
+
+form.addEventListener("submit", handleSubmit);
