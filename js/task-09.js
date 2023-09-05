f61067c8c7685bffad9297bc12703217bb272f07
azelenin057 <yazel057@gmail.com>
2023-09-05 20:34:11 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.body;
+const span = document.querySelector("span.color");
+const btn = document.querySelector(".change-color");
+
+btn.addEventListener("click", (e) => {
+  const randomColor = getRandomHexColor();
+  span.textContent = randomColor;
+  body.style.backgroundColor = randomColor;
+});
